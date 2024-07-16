835f5fd2df7a5e75c2ec54955acec5849e909dcc
Felipe Souza <felipedb91@gmail.com>
2024-07-15 23:27:53 -0300

    feat: load citys form a state

--- src/main/java/dev/fsantana/view/CityResource.java
+++ src/main/java/dev/fsantana/view/CityResource.java
@@ -1,9 +1,13 @@
 package dev.fsantana.view;
 
-import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.resteasy.reactive.RestPath;
 
+import dev.fsantana.domain.model.City;
+import dev.fsantana.service.FindCityService;
 import dev.fsantana.view.dto.CityDTO;
 import dev.fsantana.view.dto.StateDTO;
 import jakarta.ws.rs.GET;
@@ -16,11 +20,16 @@ import jakarta.ws.rs.core.Response;
 @Produces(MediaType.APPLICATION_JSON)
 public class CityResource {
 
+  @RestClient
+  FindCityService findCityService;
+
   @GET
   @Path("{state}")
   public Response getCitiesByState(@RestPath("state") String state) {
-    System.out.println(state);
-    return Response.ok(Arrays.asList(new CityDTO("Rio de janeiro", new StateDTO("RJ", "Rio de janeiro")))).build();
+    List<City> cityByState = findCityService.findCityByState(state);
+    List<CityDTO> cities = cityByState.stream().map(city -> new CityDTO(city.nome, new StateDTO(state, null)))
+        .collect(Collectors.toList());
+    return Response.ok(cities).build();
   }
 
 }
