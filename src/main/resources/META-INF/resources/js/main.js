394d466d3a7f9d6debf7a33744bd751d194b075f
Felipe Souza <felipedb91@gmail.com>
2024-07-15 23:26:10 -0300

    feat: add select box from city

--- src/main/resources/META-INF/resources/js/main.js
+++ src/main/resources/META-INF/resources/js/main.js
@@ -2,6 +2,22 @@
 
 $(document).ready(function(){
   $(".currency-format").inputmask("currency");  //static mask
-});
 
-$
+  // show city field when state change
+
+  $("#state").on('change', function () {
+    var selectedState = $('#state :selected').val()
+    console.log(selectedState)
+    $.ajax({
+      url: "/cities/"+selectedState,
+      success: function (result) {
+        var citiesOptions = result.map(item =>
+          "<option value=\""+item.fullName+ "\"" +">" + item.fullName + "</option>")
+          $('#city').append(citiesOptions)
+
+      }
+    })
+
+    $('#city-container').removeClass('d-none')
+  })
+});
