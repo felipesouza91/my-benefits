6880184bd194f4e9a88b8f1ab32f385bd145ef81
Felipe Souza <felipedb91@gmail.com>
2024-07-16 21:15:15 -0300

    refactor: change class to be a entity

--- src/main/java/dev/fsantana/domain/model/City.java
+++ src/main/java/dev/fsantana/domain/model/City.java
@@ -1,6 +1,47 @@
 package dev.fsantana.domain.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cities")
 public class City {
-  public Integer id;
-  public String nome;
+  @Id
+  private Long id;
+
+  @Column(nullable = false, unique = true)
+  private String name;
+
+  @ManyToOne()
+  @JoinColumn(name = "state_id", referencedColumnName = "id")
+  private State state;
+
+  public City() {
+  }
+
+  public City(Long id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
 }
--- src/main/java/dev/fsantana/domain/model/State.java
+++ src/main/java/dev/fsantana/domain/model/State.java
@@ -1,7 +1,54 @@
 package dev.fsantana.domain.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "states")
 public class State {
-  public Integer id;
-  public String sigla;
-  public String nome;
+
+  @Id
+  private Long id;
+
+  @Column(name = "short_name", nullable = false, unique = true)
+  private String shortName;
+
+  @Column(name = "full_name", nullable = false, unique = true)
+  private String fullName;
+
+  public State() {
+  }
+
+  public State(Long id, String shortName, String fullName) {
+    this.id = id;
+    this.shortName = shortName;
+    this.fullName = fullName;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getShortName() {
+    return shortName;
+  }
+
+  public void setShortName(String shortName) {
+    this.shortName = shortName;
+  }
+
+  public String getFullName() {
+    return fullName;
+  }
+
+  public void setFullName(String fullName) {
+    this.fullName = fullName;
+  }
+
 }
