aaf5857714493eaf27215e1272e595f9b8750e97
Felipe Souza <felipedb91@gmail.com>
2024-07-16 23:21:02 -0300

    feat: create method to return a state by short name

--- src/main/java/dev/fsantana/domain/repositories/StateRepository.java
+++ src/main/java/dev/fsantana/domain/repositories/StateRepository.java
@@ -1,10 +1,16 @@
 package dev.fsantana.domain.repositories;
 
+import java.util.Optional;
+
 import dev.fsantana.domain.model.State;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class StateRepository implements PanacheRepository<State> {
+
+  public Optional<State> findByShortName(String stateShortName) {
+    return this.find("shortName", stateShortName).firstResultOptional();
+  }
 
 }
