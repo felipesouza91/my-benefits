9e98e502246e9a619433f174a19bb309f694a94f
Felipe Souza <felipedb91@gmail.com>
2024-07-16 23:21:27 -0300

    feat: create method to return cities by state

--- src/main/java/dev/fsantana/domain/repositories/CityRepository.java
+++ src/main/java/dev/fsantana/domain/repositories/CityRepository.java
@@ -1,10 +1,16 @@
 package dev.fsantana.domain.repositories;
 
+import java.util.List;
+
 import dev.fsantana.domain.model.City;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class CityRepository implements PanacheRepository<City> {
+
+  public List<City> findByState(String stateShortName) {
+    return find("state.shortName", stateShortName).list();
+  }
 
 }
