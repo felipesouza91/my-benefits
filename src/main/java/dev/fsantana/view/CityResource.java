f582b89ab27840957a9d5e5a200c5fb1398a5197
Felipe Souza <felipedb91@gmail.com>
2024-07-16 23:23:04 -0300

    feat: implements resources

--- src/main/java/dev/fsantana/view/CityResource.java
+++ src/main/java/dev/fsantana/view/CityResource.java
@@ -3,13 +3,13 @@ package dev.fsantana.view;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.resteasy.reactive.RestPath;
 
 import dev.fsantana.domain.model.City;
-import dev.fsantana.service.FindCityService;
+import dev.fsantana.domain.usecases.FindCityByStateUseCase;
 import dev.fsantana.view.dto.CityDTO;
 import dev.fsantana.view.dto.StateDTO;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -20,14 +20,16 @@ import jakarta.ws.rs.core.Response;
 @Produces(MediaType.APPLICATION_JSON)
 public class CityResource {
 
-  @RestClient
-  FindCityService findCityService;
+  @Inject
+  private FindCityByStateUseCase findCityByStateUseCase;
 
   @GET
   @Path("{state}")
   public Response getCitiesByState(@RestPath("state") String state) {
-    List<City> cityByState = findCityService.findCityByState(state);
-    List<CityDTO> cities = cityByState.stream().map(city -> new CityDTO(city.nome, new StateDTO(state, null)))
+    List<City> cityByState = findCityByStateUseCase.execute(state);
+    List<CityDTO> cities = cityByState.stream()
+        .map(city -> new CityDTO(city.getName(),
+            new StateDTO(city.getState().getShortName(), city.getState().getFullName())))
         .collect(Collectors.toList());
     return Response.ok(cities).build();
   }
